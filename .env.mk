17a1fd03b38c0d4ffa12aad0775c74f5562c7560
Ingy döt Net <ingy@ingy.net>
2013-05-08 21:11:41 -0700

    s/egrep/grep/

--- .env.mk
+++ .env.mk
@@ -1,6 +1,6 @@
 SOURCE_TESTML := ../testml-tml
 SOURCE_TML_FILES := $(shell find $(SOURCE_TESTML) -name *.tml \
-    | egrep -v 'comments' \
+    | grep -v 'comments' \
     | grep -v 'data' \
     | grep -v 'external[12]' \
     | grep -v 'syntax2\?' \
